b6ceb39eb0cec9f5b60351db06db5aa147e50a35
leetenc <lchin@imsystems.com.au>
2021-01-11 14:06:07 +1100

    the 2nd commit

--- ace.rb
+++ ace.rb
@@ -1,8 +1,9 @@
 echo "# lewagonprep" >> README.md
-git init
+jjgit init
 git add README.md
 git commit -m "first commit"
 git branch -M main
 git remote add origin git@github.com:leetenc/lewagonprep.git
 git push -u origin main
+jkhjk
                 
