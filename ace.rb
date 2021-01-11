ac64f20864e27db8d8010fe064316064814e7b33
leetenc <lchin@imsystems.com.au>
2021-01-11 14:03:18 +1100

    the first commit

---
+++ ace.rb
@@ -0,0 +1,8 @@
+echo "# lewagonprep" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin git@github.com:leetenc/lewagonprep.git
+git push -u origin main
+                
